ea01abf39c628aa8dde97034ea1a2a9cd9c57470
Marcus Deuss <marcus.deuss@me.com>
2020-05-11 17:04:53 +0200

    added settings bundle to work in iOS and mac os

--- Inventory/AppDelegate.swift
+++ Inventory/AppDelegate.swift
@@ -143,6 +143,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         } */
         WatchSessionManager.sharedManager.startSession()
         
+        /** The registration domain is volatile.  It does not persist across launches.
+            You must register your defaults at each launch; otherwise you will get (system) default values when accessing
+            the values of preferences the user (via the Settings app) or your app (via set*:forKey:) has not modified.
+            Registering a set of default values ensures that your app always has a known good set of values to operate on.
+        */
+        registerDefaultsFromSettingsBundle()
+        
         return true
     }
 
@@ -532,5 +539,31 @@ extension AppDelegate {
         let activityViewController:UIActivityViewController = UIActivityViewController(activityItems: shareItems as [Any], applicationActivities: nil)
 
         window?.rootViewController?.present(activityViewController, animated: true, completion: nil)
+    }
+    
+    // MARK: - Preferences
+    
+    enum BackgroundColors: Int {
+        case blue = 1
+        case teal = 2
+        case indigo = 3
+    }
+    
+    // Locates the file representing the root page of the settings for this app and registers the loaded values as the app's defaults.
+    func registerDefaultsFromSettingsBundle() {
+        let settingsUrl =
+            Bundle.main.url(forResource: "Settings", withExtension: "bundle")!.appendingPathComponent("Root.plist")
+        let settingsPlist = NSDictionary(contentsOf: settingsUrl)!
+        if let preferences = settingsPlist["PreferenceSpecifiers"] as? [NSDictionary] {
+            var defaultsToRegister = [String: Any]()
+    
+            for prefItem in preferences {
+                guard let key = prefItem["Key"] as? String else {
+                    continue
+                }
+                defaultsToRegister[key] = prefItem["DefaultValue"]
+            }
+            UserDefaults.standard.register(defaults: defaultsToRegister)
+        }
     }
 }
